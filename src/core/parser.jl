60d0a6bb0cb036ade64830c4626f6af5879affc0
Li Huang <lihuang.dmft@gmail.com>
2020-11-08 23:05:31 +0800

    try to implement parser.jl

--- src/core/parser.jl
+++ src/core/parser.jl
@@ -1,6 +1,22 @@
 using TOML
 
+"""
+    parse_zen_config(f::AbstractString)
+
+Parse the configuration file (toml format)
+"""
 function parse_zen_config(f::AbstractString)
     dict = TOML.parsefile(f)
-    @show dict
+    case = dict["case"]
+    dft = dict["dft"]
+    dmft = dict["dmft"]
+    solver = dict["solver"]
+    adaptor = dict["adaptor"]
+    impurity = dict["impurity"]
+    @show case
+    @show dft
+    @show dmft
+    @show solver
+    @show adaptor
+    @show impurity
 end
