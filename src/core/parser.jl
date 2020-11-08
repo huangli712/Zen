3ac192c5e9d3540a58dbe6bc8ca544c12d7b8ec4
Li Huang <lihuang.dmft@gmail.com>
2020-11-08 22:56:24 +0800

    update zen.jl and parser.jl

--- src/core/parser.jl
+++ src/core/parser.jl
@@ -1,12 +1,6 @@
-module ZenParser
-
 using TOML
-
-export parse_zen_config
 
 function parse_zen_config(f::AbstractString)
     dict = TOML.parsefile(f)
     @show dict
-end
-
 end
---
+++ src/core/zen.jl
@@ -0,0 +1,6 @@
+module Zen
+
+include("parser.jl")
+export parse_zen_config
+
+end
