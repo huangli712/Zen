37188ce0bdeca7e3926f78a2d8075a9c3ab810a3
Li Huang <lihuang.dmft@gmail.com>
2020-11-14 17:03:04 +0800

    rename parse_config() to parse_toml()

--- src/core/parser.jl
+++ src/core/parser.jl
@@ -5,13 +5,21 @@ using TOML
 
 Parse the configuration file (toml format)
 """
-function parse_toml(f::AbstractString, key::AbstractString)
-    dict = TOML.parsefile(f)
+function parse_toml(f::AbstractString, key::AbstractString, necessary::Bool)
+    if isfile(f)
+        dict = TOML.parsefile(f)
 
-    if haskey(dict, key)
-        dict[key]
+        if haskey(dict, key)
+            dict[key]
+        else
+            error("Do not have this key: $key in file: $f")
+        end
     else
-        error("Do not have this key: $key")
+        if necessary
+            error("Please make sure that the file $f really exists")
+        else
+            nothing
+        end
     end
 end
 
