95b579498f531ad181a9badc49a31179a100f9ab
Li Huang <lihuang.dmft@gmail.com>
2020-11-30 23:29:37 +0800

    add parse_dict()

--- src/julia/parser.jl
+++ src/julia/parser.jl
@@ -40,11 +40,46 @@ end
 
 """
     parse_dict(cfg::Dict{String,Any})
+
+Copy parameters from cfg to PCASE, PDFT, PDMFT, PIMP, and PSOLVER
 """
 function parse_dict(cfg::Dict{String,Any})
     case = cfg["case"]
+    PCASE["case"][1] = case
+
     dft = cfg["dft"]
+    for key in keys(dft)
+        if haskey(PDFT, key)
+            PDFT[key][1] = dft[key]
+        else
+            error("Sorry, $key is not supported currently")
+        end
+    end
+
     dmft = cfg["dmft"]
+    for key in keys(dmft)
+        if haskey(PDMFT, key)
+            PDMFT[key][1] = dmft[key]
+        else
+            error("Sorry, $key is not supported currently")
+        end
+    end
+
     impurity = cfg["impurity"]
+    for key in keys(impurity)
+        if haskey(PIMP, key)
+            PIMP[key][1] = impurity[key]
+        else
+            error("Sorry, $key is not supported currently")
+        end
+    end
+
     solver = cfg["solver"]
+    for key in keys(solver)
+        if haskey(PSOLVER, key)
+            PSOLVER[key][1] = solver[key]
+        else
+            error("Sorry, $key is not supported currently")
+        end
+    end
 end
