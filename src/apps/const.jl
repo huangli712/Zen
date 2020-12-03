653ea9ead2531202c1c239a1ec545048998e7b9c
Li Huang <lihuang.dmft@gmail.com>
2020-12-03 16:10:13 +0800

    add some numerical constants in const.jl

--- src/apps/const.jl
+++ src/apps/const.jl
@@ -1,14 +1,51 @@
+"""
+    I32, I64
 
+Alias of integer type
+"""
 const I32 = Int32
 const I64 = Int64
 
+"""
+    F32, F64
+
+Alias of float type
+"""
 const F32 = Float32
 const F64 = Float64
 
+"""
+    C32, C64
+
+Alias of complex type
+"""
 const C32 = ComplexF32
 const C64 = ComplexF64
 
+"""
+    __libname__
+
+Name of this julia package
+"""
 const __libname__ = "ZEN"
-const __version__ = "0.0.4@d" 
+
+"""
+    __version__
+
+Version of this julia package
+"""
+const __version__ = "0.0.4@d"
+
+"""
+    __release__
+
+Release date of this julia package
+"""
 const __release__ = "2020/12"
+
+"""
+    __authors__
+
+Authors of this julia package
+"""
 const __authors__ = ["Li Huang (lihuang@gmail.com)"]
