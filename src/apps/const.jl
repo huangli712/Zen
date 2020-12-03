a65c03d2e2a441e024b7c61e41632b2673b50cbc
Li Huang <lihuang.dmft@gmail.com>
2020-12-03 16:05:36 +0800

    add some numerical constants in const.jl

--- src/apps/const.jl
+++ src/apps/const.jl
@@ -7,3 +7,8 @@ const F64 = Float64
 
 const C32 = ComplexF32
 const C64 = ComplexF64
+
+const __libname__ = "ZEN"
+const __version__ = "0.0.4@d" 
+const __release__ = "2020/12"
+const __authors__ = ["Li Huang (lihuang@gmail.com)"]
