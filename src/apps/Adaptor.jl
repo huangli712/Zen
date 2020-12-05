2dd9a19576d867586286051de9aac9469f7e2d6a
Li Huang <lihuang.dmft@gmail.com>
2020-12-05 21:43:17 +0800

    update Adaptor.jl

--- src/apps/Adaptor.jl
+++ src/apps/Adaptor.jl
@@ -10,3 +10,51 @@ export from_chgcar
 export to_chgcar
 
 end
+function from_poscar(f::AbstractString)
+end
+
+function from_projcar(f::AbstractString)
+end
+
+function from_locproj(f::AbstractString)
+end
+
+"""
+    from_ibzkpt
+
+Reading IBZKPT, return k-mesh and k-weight
+"""
+function from_ibzkpt(f::AbstractString)
+    fin = open(f * "/IBZKPT", "r")
+
+    readline(fin)
+    str = readline(fin)
+    nkpt = parse(Int64, str)
+    readline(fin)
+
+    kmesh = zeros(Float64, nkpt, 3)
+    weight = zeros(Float64, nkpt) 
+
+    i = 0
+    for line in eachline(fin)
+        substr = split(line, " ", keepempty = false)
+        subarr = map(x -> parse(Float64, x),  substr[1:3]) 
+        i = i + 1
+        kmesh[i,1:3] = subarr
+        weight[i] = parse(Int64, substr[4])
+        println(kmesh[i,:], " ", weight[i])
+    end
+    @assert( i === nkpt )
+    println(sum(weight))
+
+    close(fin)
+end
+
+function from_eigenval(f::AbstractString)
+end
+
+function from_chgcar(f::AbstractString)
+end
+
+function to_chgcar()
+end
