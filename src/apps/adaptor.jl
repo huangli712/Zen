431171bb51f4490a7a73044a0b479b249399e9a4
Li Huang <lihuang.dmft@gmail.com>
2020-12-11 16:06:43 +0800

    try to implement vaspio_locproj()

--- src/apps/adaptor.jl
+++ src/apps/adaptor.jl
@@ -139,7 +139,9 @@ function vaspio_locproj(f::AbstractString, read_param_only::Bool = false)
     # extract number of spins (nspin), number of k-points (nkpt),
     # number of bands (nband), and number of projectors (nproj)
     arr = line_to_array(fin)
-    nspin, nkpt, nband, nproj = tuple(map(x -> parse(I64,x), arr[1:4])...)
+    nspin, nkpt, nband, nproj = tuple(parse.(I64, arr[1:4])...)
+    @show nspin, nkpt, nband, nproj
+    exit(-1)
 
     # find out how many sites are there (nsite)
     sites = zeros(I64, nproj)
