3d99bfa8f17efaa44ddb0181ecc149d31189e5b4
Li Huang <lihuang.dmft@gmail.com>
2020-12-15 22:09:58 +0800

    update util.jl

--- src/apps/util.jl
+++ src/apps/util.jl
@@ -1,3 +1,13 @@
+#
+# project : pansy
+# source  : util.jl
+# author  : Li Huang (lihuang.dmft@gmail.com)
+# status  : unstable
+# comment :
+#
+# last modified: 2020/12/15
+#
+
 """
     require()
 
