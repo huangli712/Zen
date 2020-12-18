c1e4c153f424c14fe9ca27b8a4e98ade8d449376
Li Huang <lihuang.dmft@gmail.com>
2020-12-18 11:21:40 +0800

    try to implement plo_ovlp()

--- src/apps/plo.jl
+++ src/apps/plo.jl
@@ -8,8 +8,18 @@
 # last modified: 2020/12/18
 #
 
-function plo_ovlp()
+"""
+    plo_ovlp()
+"""
+function plo_ovlp(chipsi::Array{C64,4}, weight::Array{F64,1}, occupy::Array{F64,3})
+    # extract some key parameters
+    nproj, nband, nkpt, nspin = size(chipsi)
+
+    ovlp = zeros(F64, nproj, nproj, nspin)
 end
 
+"""
+    plo_dm()
+"""
 function plo_dm()
 end
--- src/apps/test.jl
+++ src/apps/test.jl
@@ -3,21 +3,38 @@
 include("Zen.jl")
 using .Zen
 
+# test vaspio_kmesh() and irio_kmesh()
+#--------------------------------------
 #kmesh, weight = vaspio_kmesh(joinpath(pwd(), "dft"))
 #irio_kmesh(pwd(), kmesh, weight)
 
+# test vaspio_tetra() and irio_tetra()
+#--------------------------------------
 #volt, itet = vaspio_tetra(joinpath(pwd(), "dft"))
 #irio_tetra(pwd(), volt, itet)
 
+# test vaspio_eigen() and irio_eigen()
+#--------------------------------------
 #enk, occupy = vaspio_eigen(joinpath(pwd(), "dft"))
 #irio_eigen(pwd(), enk, occupy)
 
+# test vaspio_projs() and irio_projs()
+#--------------------------------------
 #chipsi1 = vaspio_projs(joinpath(pwd(), "dft"))
 #chipsi2 = vaspio_projs(joinpath(pwd(), "dft"), false)
 #irio_projs(pwd(), chipsi2)
 
+# test vaspio_fermi() and irio_fermi()
+#--------------------------------------
 #fermi = vaspio_fermi(joinpath(pwd(), "dft"))
 #irio_fermi(pwd(), fermi)
 
-#nsorts, natoms, symbols, atom_list, posi_list = vaspio_poscar(pwd() * "/dft")
+# test vaspio_lattice()
+#--------------------------------------
+#nsorts, natoms, symbols, atom_list, posi_list = vaspio_lattice(joinpath(pwd(), "dft"))
 #@show nsorts, natoms, symbols, atom_list, posi_list
+
+chipsi = vaspio_projs(joinpath(pwd(), "dft"), false)
+kmesh, weight = vaspio_kmesh(joinpath(pwd(), "dft"))
+enk, occupy = vaspio_eigen(joinpath(pwd(), "dft"))
+plo_ovlp(chipsi, weight, occupy)
