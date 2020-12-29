d26ee3dc81314ebc812b379cb9d8378ea62e8485
Li Huang <lihuang.dmft@gmail.com>
2020-12-29 11:24:14 +0800

    add more comments

---
+++ src/apps/dos.jl
@@ -0,0 +1,9 @@
+#
+# project : pansy
+# source  : dos.jl
+# author  : Li Huang (lihuang.dmft@gmail.com)
+# status  : unstable
+# comment :
+#
+# last modified: 2020/12/29
+#
---
+++ src/apps/tetra.jl
@@ -0,0 +1,9 @@
+#
+# project : pansy
+# source  : tetra.jl
+# author  : Li Huang (lihuang.dmft@gmail.com)
+# status  : unstable
+# comment :
+#
+# last modified: 2020/12/29
+#
