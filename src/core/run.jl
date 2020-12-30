3633b5624f345f637243430a5120ff71030522c9
Li Huang <lihuang.dmft@gmail.com>
2020-12-30 23:43:52 +0800

    improve run.jl

--- src/core/run.jl
+++ src/core/run.jl
@@ -27,16 +27,16 @@ overview()
 message("ZEN", "PARSER")
 #
 # S02.1: parse the case.toml file to extract configuration parameters
-cfg = parse_toml(query_args(), true)
 println("Extract configuration parameters from $(query_args())")
+cfg = parse_toml(query_args(), true)
 #
 # S02.2: build the configuration dictionaries
-renew_config(cfg)
 println("Update configuration dictionaries")
+renew_config(cfg)
 #
 # S02.3: validate the configuration dictionaries
-check_config()
 println("Verify configuration dictionaries\n")
+check_config()
 
 # S03: write the configuration parameters to stdout
 message("ZEN", "PARAMETER")
@@ -60,16 +60,16 @@ list_solver()
 message("ZEN", "PREPARATION")
 #
 # S04.1: check the input files (which are essential for the calculation)
-query_inps()
 println("Check essential input files")
+query_inps()
 #
 # S04.2: prepare the working directories
-make_trees()
 println("Create working directories")
+make_trees()
 #
 # S04.3: create an IterInfo object
-it = IterInfo()
 println("Create self-consistent iterator")
+it = IterInfo()
 #
 # S04.4: check self-consistent mode
 mode = _m("mode") === 1 ? "one-shot" : "fully self-consistent"
@@ -94,16 +94,16 @@ if _m("mode") === 1
     message("ZEN", "DFT")
     #
     # S05.1: prepare and check essential files for the dft engine 
-    dft_init(it)
     println("Initialize everything needed by the dft engine")
+    dft_init(it)
     #
     # S05.2: perform a self-consitent calculation at the dft level
-    dft_run(it)
     println("Launch the dft engine")
+    dft_run(it)
     #
     # S05.3: backup the output files of the dft engine
-    dft_save(it)
     println("Save the output files\n")
+    dft_save(it)
 
     # we want better optimal projectors
     # in the previous dft run, initial fermi level = 0 -> wrong energy
@@ -116,24 +116,52 @@ if _m("mode") === 1
         message("ZEN", "DFT")
         #
         # S06.1: prepare and check essential files for the dft engine 
-        dft_init(it)
         println("Initialize everything needed by the dft engine")
+        dft_init(it)
         #
         # S06.2: perform a self-consitent calculation at the dft level
-        dft_run(it)
         println("Launch the dft engine")
+        dft_run(it)
         #
         # S06.3: backup the output files of the dft engine
-        dft_save(it)
         println("Save the output files\n")
+        dft_save(it)
 
     end
 
-    # S07:
+#
+# remarks:
+#
+# the key Kohn-Sham data inclue lattice structures, k-mesh and its weights,
+# tetrahedra data, eigenvalues, raw projectors, and fermi level, etc. at
+# first the adaptor will read in these data from the output files of dft
+# engine. and then it will process the raw projectors (parse, label, group,
+# filter, and rotate). finally, the adaptor will write down the processed
+# data to some files within the IR format. 
+#
+
+    # S07: To bridge the gap between dft engine and dmft engine by adaptor
     message("ZEN", "ADAPTOR")
+    #
+    # S07.1: prepare and check essential files for the adaptor
+    println("Initialize everything needed by the adaptor")
     adaptor_init(it)
+    #
+    # S07.2: launch the adaptor. it will read the Kohn-Sham data from the
+    # dft engine, postprocess them, and then write them to external files
+    # with the IR format 
+    println("Launch the adaptor")
     adaptor_run(it)
+    #
+    # S07.3: backup the output files of the adaptor
+    println("Save the output files\n")
     adaptor_save(it)
+
+    for iter = 1:_m("niter")
+        message("ZEN", "ITER : $iter")
+        message("ZEN", "DMFT1")
+        message("ZEN", "SOLVER")
+    end
 
 else
     sorry()
