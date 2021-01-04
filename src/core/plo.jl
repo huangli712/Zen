b9ccc07f0f9c3b3ebdc0b3565cceae99c788a204
Li Huang <lihuang.dmft@gmail.com>
2021-01-04 11:26:18 +0800

    improve plo.jl

--- src/core/plo.jl
+++ src/core/plo.jl
@@ -15,7 +15,7 @@ Use the information contained in the PIMP dict to further complete
 the PrGroup struct
 """
 function plo_group(PG::Array{PrGroup,1})
-    # check the parameters contained in PIMP dict
+    # additional check for the parameters contained in PIMP dict
     @assert _i("nsite") === length(_i("atoms"))
     @assert _i("nsite") === length(_i("shell"))
 
@@ -104,6 +104,7 @@ function plo_rotate(PG::Array{PrGroup,1}, chipsi::Array{C64,4})
         # determine how many projectors should be included in this group
         # according to the rotation matrix (transformation matrix)
         ndim = size(PG[i].Tr)[1]
+        # create PrGroupT and push it into PGT array
         push!(PGT, PrGroupT(PG[i].site, PG[i].l, ndim, PG[i].corr, PG[i].shell))
     end
 
@@ -129,6 +130,7 @@ function plo_rotate(PG::Array{PrGroup,1}, chipsi::Array{C64,4})
     chipsi_r = zeros(C64, nproj_, nband, nkpt, nspin)
 
     # perform rotation or transformation
+    # note: PG[i].Tr must be a matrix. its size must be (q2 - q1 + 1, p2 - p1 + 1)
     for s = 1:nspin
         for k = 1:nkpt
             for b = 1:nband
@@ -185,16 +187,17 @@ function plo_window(enk::Array{F64,3}, emax::F64, emin::F64, chipsi::Array{C64,4
             @assert ib1 <= ib2
 
             # save the boundaries
+            # ib1 and ib2 mean the lower and upper boundaries, respectively
             ib_window[k, s, 1] = ib1
             ib_window[k, s, 2] = ib2
         end
     end
 
-    # try to find out the minimum and maximum band indices
+    # try to find out the global minimum and maximum band indices
     ib_min = minimum(ib_window[:, :, 1])
     ib_max = maximum(ib_window[:, :, 2])
 
-    # try to find out the maximum number of selected bands
+    # try to find out the maximum number of selected bands in the window
     nbmax = ib_max - ib_min + 1
 
     # extract some key parameters
@@ -232,6 +235,7 @@ function plo_orthog(window::Array{I64,3}, PGT::Array{PrGroupT,1}, chipsi::Array{
     # create arrays
     TmpMat = zeros(C64, nproj, nband)
 
+    # loop over spins and kpoints
     for s = 1:nspin
         for k = 1:nkpt
             # reset tmp matrix
@@ -249,7 +253,7 @@ function plo_orthog(window::Array{I64,3}, PGT::Array{PrGroupT,1}, chipsi::Array{
                 # extract the desired subarray
                 TmpMat[q1:q2, 1:nb] = chipsi[q1:q2, 1:nb, k, s]
 
-                # diagonalize it
+                # orthogonalize it
                 STmpMat = plo_diag(TmpMat[q1:q2, 1:nb])
 
                 # copy it back to original array
@@ -269,7 +273,7 @@ function plo_diag(M::Array{C64,2})
     ovlp = M * M'
     @assert ishermitian(ovlp)
 
-    # diagonalize the overlap matrix
+    # diagonalize the overlap matrix, return eigenvalues and eigenvectors
     vals, vecs = eigen(Hermitian(ovlp))
     @assert all(vals .> 0)
 
@@ -284,7 +288,7 @@ end
 """
     plo_ovlp(chipsi::Array{C64,4}, weight::Array{F64,1})
 
-Calculate the overlap out of projectors. General version
+Calculate the overlap matrix out of projectors. General version
 """
 function plo_ovlp(chipsi::Array{C64,4}, weight::Array{F64,1})
     # extract some key parameters
@@ -309,7 +313,7 @@ end
 """
     plo_ovlp(PGT::Array{PrGroupT,1}, chipsi::Array{C64,4}, weight::Array{F64,1})
 
-Calculate the overlap out of projectors. The overlap matrix is block-diagonal
+Calculate the overlap matrix out of projectors. It should be block-diagonal
 """
 function plo_ovlp(PGT::Array{PrGroupT,1}, chipsi::Array{C64,4}, weight::Array{F64,1})
     # extract some key parameters
@@ -345,12 +349,12 @@ function plo_dm(chipsi::Array{C64,4}, weight::Array{F64,1}, occupy::Array{F64,3}
     nproj, nband, nkpt, nspin = size(chipsi)
 
     # evaluate spin factor
-    sf = (nspin == 1 ? 2 : 1)
+    sf = (nspin === 1 ? 2 : 1)
 
     # create density matrix array
     dm = zeros(F64, nproj, nproj, nspin)
 
-    # build overlap array
+    # build density matrix array
     for s = 1:nspin
         for k = 1:nkpt
             wght = weight[k] / nkpt * sf
@@ -367,7 +371,7 @@ end
 """
     plo_dm(bmin::I64, bmax::I64, PGT::Array{PrGroupT,1}, chipsi::Array{C64,4}, weight::Array{F64,1}, occupy::Array{F64,3})
 
-Calculate the density matrix out of projectors. The overlap matrix is block-diagonal
+Calculate the density matrix out of projectors. It should be block-diagonal
 """
 function plo_dm(bmin::I64, bmax::I64, PGT::Array{PrGroupT,1}, chipsi::Array{C64,4}, weight::Array{F64,1}, occupy::Array{F64,3})
     # extract some key parameters
@@ -377,12 +381,12 @@ function plo_dm(bmin::I64, bmax::I64, PGT::Array{PrGroupT,1}, chipsi::Array{C64,
     @assert nband === bmax - bmin + 1
 
     # evaluate spin factor
-    sf = (nspin == 1 ? 2 : 1)
+    sf = (nspin === 1 ? 2 : 1)
 
     # create density matrix array
     dm = zeros(F64, nproj, nproj, nspin)
 
-    # build overlap array
+    # build density matrix array
     for s = 1:nspin
         for k = 1:nkpt
             wght = weight[k] / nkpt * sf
@@ -429,8 +433,8 @@ function view_ovlp(ovlp::Array{F64,3})
     println("<- Overlap Matrix ->")
     for s = 1:nspin
         println("Spin: $s")
-        for p1 = 1:nproj
-            foreach(x -> @printf("%12.7f", x), ovlp[p1, :, s])
+        for p = 1:nproj
+            foreach(x -> @printf("%12.7f", x), ovlp[p, :, s])
             println()
         end
     end
@@ -439,7 +443,7 @@ end
 """
     view_ovlp(PGT::Array{PrGroupT,1}, ovlp::Array{F64,3})
 
-Output the overlap matrix. The overlap matrix is block-diagonal
+Output the overlap matrix. It should be block-diagonal
 """
 function view_ovlp(PGT::Array{PrGroupT,1}, ovlp::Array{F64,3})
     # extract some key parameters
@@ -450,7 +454,7 @@ function view_ovlp(PGT::Array{PrGroupT,1}, ovlp::Array{F64,3})
     for s = 1:nspin
         println("Spin: $s")
         for p in eachindex(PGT)
-            println("site -> $(PGT[p].site) l -> $(PGT[p].l)")
+            println("site -> $(PGT[p].site) l -> $(PGT[p].l) shell -> $(PGT[p].shell)")
             q1 = PGT[p].Pr[1]
             q2 = PGT[p].Pr[end]
             for q = q1:q2
@@ -474,8 +478,8 @@ function view_dm(dm::Array{F64,3})
     println("<- Density Matrix ->")
     for s = 1:nspin
         println("Spin: $s")
-        for p1 = 1:nproj
-            foreach(x -> @printf("%12.7f", x), dm[p1, :, s])
+        for p = 1:nproj
+            foreach(x -> @printf("%12.7f", x), dm[p, :, s])
             println()
         end
     end
@@ -484,7 +488,7 @@ end
 """
     view_dm(PGT::Array{PrGroupT,1}, dm::Array{F64,3})
 
-Output the density matrix. The density matrix is block-diagonal
+Output the density matrix. It should be block-diagonal
 """
 function view_dm(PGT::Array{PrGroupT,1}, dm::Array{F64,3})
     # extract some key parameters
@@ -495,7 +499,7 @@ function view_dm(PGT::Array{PrGroupT,1}, dm::Array{F64,3})
     for s = 1:nspin
         println("Spin: $s")
         for p in eachindex(PGT)
-            println("site -> $(PGT[p].site) l -> $(PGT[p].l)")
+            println("site -> $(PGT[p].site) l -> $(PGT[p].l) shell -> $(PGT[p].shell)")
             q1 = PGT[p].Pr[1]
             q2 = PGT[p].Pr[end]
             for q = q1:q2
