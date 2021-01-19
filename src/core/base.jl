3b0482e9d7ed0b4c37e8bd48f14e4b571ae4019b
Li Huang <lihuang.dmft@gmail.com>
2021-01-19 22:03:32 +0800

    fix the comments

--- src/core/base.jl
+++ src/core/base.jl
@@ -1,11 +1,11 @@
 #
-# project : pansy
-# source  : base.jl
-# author  : Li Huang (lihuang.dmft@gmail.com)
-# status  : unstable
-# comment :
+# Project : Pansy
+# Source  : base.jl
+# Author  : Li Huang (lihuang.dmft@gmail.com)
+# Status  : Unstable
+# Comment :
 #
-# last modified: 2021/01/19
+# Last modified: 2021/01/19
 #
 
 #
