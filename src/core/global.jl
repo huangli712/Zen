12e7d69580b64abda12bce24294362cc71f789ee
Li Huang <lihuang.dmft@gmail.com>
2021-01-27 18:44:35 +0800

    add N32, N64, R32, and R64

--- src/core/global.jl
+++ src/core/global.jl
@@ -5,7 +5,7 @@
 # Status  : stable
 # Comment :
 #
-# Last modified: 2021/01/25
+# Last modified: 2021/01/27
 #
 
 """
@@ -31,6 +31,22 @@ Alias of complex type
 """
 const C32 = ComplexF32
 const C64 = ComplexF64
+
+"""
+    R32 and R64
+
+Alias of integer and float types. Here `R` means Real.
+"""
+const R32 = Union{I32,F32}
+const R64 = Union{I64,F64}
+
+"""
+    N32 and N64
+
+Alias of integer, float, and complex types. Here `N` means Number.
+"""
+const N32 = Union{I32,F32,C32}
+const N64 = Union{I64,F64,C64}
 
 """
     __LIBNAME__
@@ -62,7 +78,7 @@ Core authors of this julia package.
 #
 # Remarks:
 #
-# The Array's element should be a NamedTuple object:
+# The Array's element should be a NamedTuple object, such as:
 #     (name = "name", email = "email").
 #
 const __AUTHORS__ = [(name = "Li Huang", email = "lihuang.dmft@gmail.com")]
