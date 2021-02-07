e3860d681ed93d8200b63c2b07da9ccb60d332b1
Li Huang <lihuang.dmft@gmail.com>
2021-02-07 12:38:18 +0800

    add comments

--- src/core/tetra.jl
+++ src/core/tetra.jl
@@ -15,11 +15,11 @@
 """
     TetraWeight
 
-Integration weights for analytical tetrahedron algorithm
+Integration weights for analytical tetrahedron algorithm.
 
-.cw -> blochl corrections for dweight
-.dw -> density of states weights at the four corners of a given tetrahedron
-.tw -> integration weights at the four corners of a given tetrahedron
+.cw -> Blochl corrections for `dw`.
+.dw -> Density of states weights at the four corners of a given tetrahedron.
+.tw -> Integration weights at the four corners of a given tetrahedron.
 """
 struct TetraWeight
     cw :: F64
