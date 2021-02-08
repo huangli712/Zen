080c519193b8ab75b1e82809c6d662be1b8ee8b8
Li Huang <lihuang.dmft@gmail.com>
2021-02-08 16:02:36 +0800

    add comments

--- src/core/Zen.jl
+++ src/core/Zen.jl
@@ -26,12 +26,18 @@ using Dates
 # The TOML.jl package is included in the standard library since v1.6.
 # So, please upgrade your julia environment if it is outdated.
 #
-# The SpecialFunctions.jl was included in the standard library for
-# old version julia. But in the newest julia, it is not included any
-# more. We have to install it by ourselves. We need this package to
-# support the `erf` function.
+# We need this package to parse the configuration file.
 #
 using TOML
+#
+# Remarks:
+#
+# The SpecialFunctions.jl was included in the standard library for
+# old version julia. But in the newest julia, it is not included any
+# more. We have to install it by ourselves. 
+#
+# We need this package to support the `erf` function.
+#
 using SpecialFunctions
 
 #
