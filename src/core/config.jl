8be8f77bfae852018bc3f26e35b438596f8345f7
Li Huang <lihuang.dmft@gmail.com>
2021-02-21 22:45:21 +0800

    fix bug in config.jl

--- src/core/config.jl
+++ src/core/config.jl
@@ -5,7 +5,7 @@
 # status  : unstable
 # comment :
 #
-# last modified: 2021/02/19
+# last modified: 2021/02/21
 #
 
 #
@@ -203,10 +203,11 @@ function chk_dict()
     @assert get_m("niter") > 0
     @assert get_m("dcount") in ("fll1", "fll2", "amf", "exact")
     @assert get_m("beta") >= 0.0
-    @assert get_m("mixer") >= 0.0 && @assert get_m("mixer") <= 1.0
+    @show get_m("mixer")
+    @assert get_m("mixer") >= 0.0 && get_m("mixer") <= 1.0
     #
     # Check impurity block
-    @assert get_i("nsite") >= 1 && @assert get_i("nsite") <= 99
+    @assert get_i("nsite") >= 1 && get_i("nsite") <= 99
     #
     # Check solver block
     @assert get_s("engine") in ("ct_hyb1", "ct_hyb2", "hub1", "norg")
