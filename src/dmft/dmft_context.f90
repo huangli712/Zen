72a1f158f0410d70545c2160d03f0d50741cdcf9
Li Huang <lihuang.dmft@gmail.com>
2021-02-23 10:08:28 +0800

    add comments

--- src/dmft/dmft_context.f90
+++ src/dmft/dmft_context.f90
@@ -1,2 +1,15 @@
+!!!-----------------------------------------------------------------------
+!!! project : gardenia
+!!! program : context    module
+!!! source  : ctqmc_context.f90
+!!! type    : module
+!!! author  : li huang (email:lihuang.dmft@gmail.com)
+!!! history : 02/23/2021 by li huang (created)
+!!!           02/23/2021 by li huang (last modified)
+!!! purpose : 
+!!! status  : unstable
+!!! comment :
+!!!-----------------------------------------------------------------------
+
   module context
   end module context
