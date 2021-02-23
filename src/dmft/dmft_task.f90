615be66ab7d9a75d41de39b8f96a9fd9aeb5edd1
Li Huang <lihuang.dmft@gmail.com>
2021-02-23 10:20:22 +0800

    add comments

--- src/dmft/dmft_task.f90
+++ src/dmft/dmft_task.f90
@@ -1,3 +1,15 @@
+!!!-----------------------------------------------------------------------
+!!! project : gardenia
+!!! program : 
+!!! source  : dmft_task.f90
+!!! type    : module
+!!! author  : li huang (email:lihuang.dmft@gmail.com)
+!!! history : 02/23/2021 by li huang (created)
+!!!           02/23/2021 by li huang (last modified)
+!!! purpose : 
+!!! status  : unstable
+!!! comment :
+!!!-----------------------------------------------------------------------
 
   subroutine task()
   end subroutine task
