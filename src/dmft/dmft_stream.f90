a61bff3b6615f0815a6a8fc778d3a6a658dc0fe6
Li Huang <lihuang.dmft@gmail.com>
2021-02-23 10:19:09 +0800

    add comments

--- src/dmft/dmft_stream.f90
+++ src/dmft/dmft_stream.f90
@@ -1,2 +1,15 @@
+!!!-----------------------------------------------------------------------
+!!! project : gardenia
+!!! program : 
+!!! source  : dmft_stream.f90
+!!! type    : module
+!!! author  : li huang (email:lihuang.dmft@gmail.com)
+!!! history : 02/23/2021 by li huang (created)
+!!!           02/23/2021 by li huang (last modified)
+!!! purpose : 
+!!! status  : unstable
+!!! comment :
+!!!-----------------------------------------------------------------------
+
   subroutine stream()
   end subroutine stream
