2959f516e2049fce37c6bb4bbbe8ce9f29c99543
Li Huang <lihuang.dmft@gmail.com>
2021-02-23 10:07:32 +0800

    add comments

--- src/dmft/dmft_control.f90
+++ src/dmft/dmft_control.f90
@@ -1,2 +1,15 @@
+!!!-----------------------------------------------------------------------
+!!! project : gardenia
+!!! program : control    module
+!!! source  : ctqmc_control.f90
+!!! type    : module
+!!! author  : li huang (email:lihuang.dmft@gmail.com)
+!!! history : 02/23/2021 by li huang (created)
+!!!           02/23/2021 by li huang (last modified)
+!!! purpose : 
+!!! status  : unstable
+!!! comment :
+!!!-----------------------------------------------------------------------
+
   module control
   end module control
