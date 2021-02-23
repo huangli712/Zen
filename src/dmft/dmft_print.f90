a80f526fa6bae61a6b0919e7ae66f19de473b906
Li Huang <lihuang.dmft@gmail.com>
2021-02-23 10:22:38 +0800

    add comments

--- src/dmft/dmft_print.f90
+++ src/dmft/dmft_print.f90
@@ -1,2 +1,15 @@
-  subroutine print()
-  end subroutine print
+!!!-----------------------------------------------------------------------
+!!! project : gardenia
+!!! program : 
+!!! source  : dmft_print.f90
+!!! type    : subroutines
+!!! author  : li huang (email:lihuang.dmft@gmail.com)
+!!! history : 02/23/2021 by li huang (created)
+!!!           02/23/2021 by li huang (last modified)
+!!! purpose : 
+!!! status  : unstable
+!!! comment :
+!!!-----------------------------------------------------------------------
+
+  subroutine dmft_print_header()
+  end subroutine dmft_print_header
