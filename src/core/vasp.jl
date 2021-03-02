fb25cedfbf4e8b6eb352d5f017587ffaa2b750d5
Li Huang <lihuang.dmft@gmail.com>
2021-03-02 21:36:29 +0800

    add comments

--- src/core/vasp.jl
+++ src/core/vasp.jl
@@ -1,11 +1,11 @@
 #
-# project : pansy
-# source  : vasp.jl
-# author  : Li Huang (lihuang.dmft@gmail.com)
-# status  : unstable
-# comment :
+# Project : Pansy
+# Source  : vasp.jl
+# Author  : Li Huang (lihuang.dmft@gmail.com)
+# Status  : Unstable
+# Comment :
 #
-# last modified: 2021/03/02
+# Last modified: 2021/03/02
 #
 
 #
