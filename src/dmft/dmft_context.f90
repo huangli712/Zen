a3420547b33af490bdd786dfaf514d5a5ff44dd5
Li Huang <lihuang.dmft@gmail.com>
2021-03-17 00:21:01 +0800

    add some modules in dmft_context.f90

--- src/dmft/dmft_context.f90
+++ src/dmft/dmft_context.f90
@@ -11,5 +11,26 @@
 !!! comment :
 !!!-----------------------------------------------------------------------
 
+  module dmft_maps
+  end module dmft_maps
+
+  module dmft_atoms
+  end module dmft_atoms
+
+  module dmft_kmesh
+  end module dmft_kmesh
+
+  module dmft_eigen
+  end module dmft_eigen
+
+  module dmft_projs
+  end module dmft_projs
+
+  module dmft_sigma
+  end module dmft_sigma
+
+  module dmft_green
+  end module dmft_green
+
   module context
   end module context
