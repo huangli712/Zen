2ddc7275413d669f8479cde88e1985eef542f99c
Li Huang <lihuang.dmft@gmail.com>
2021-03-23 22:55:49 +0800

    reset sigma.jl

--- src/core/sigma.jl
+++ src/core/sigma.jl
@@ -6,28 +6,3 @@
 #
 # Last modified: 2021/03/23
 #
-
-"""
-    sigma_init(it::IterInfo)
-"""
-function sigma_init(it::IterInfo)
-    #if it.
-end
-
-"""
-    sigma_run(it::IterInfo)
-"""
-function sigma_run(it::IterInfo)
-end
-
-"""
-    sigma_save(it::IterInfo)
-"""
-function sigma_save(it::IterInfo)
-end
-
-function sigma_zero()
-end
-
-function sigma_dcount()
-end
