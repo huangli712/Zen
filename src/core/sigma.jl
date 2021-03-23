1908158ba0677910108c4d3f294ecdf8bbce2f42
Li Huang <lihuang.dmft@gmail.com>
2021-03-23 22:48:10 +0800

    add comments

--- src/core/sigma.jl
+++ src/core/sigma.jl
@@ -7,12 +7,22 @@
 # Last modified: 2021/03/23
 #
 
+"""
+    sigma_init(it::IterInfo)
+"""
 function sigma_init(it::IterInfo)
+    if it.
 end
 
+"""
+    sigma_run(it::IterInfo)
+"""
 function sigma_run(it::IterInfo)
 end
 
+"""
+    sigma_save(it::IterInfo)
+"""
 function sigma_save(it::IterInfo)
 end
 
