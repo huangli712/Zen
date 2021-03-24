dabdb29546a3c57eecc438b32e4696301d292e5a
Li Huang <lihuang.dmft@gmail.com>
2021-03-24 10:27:57 +0800

    add comments

--- src/core/sigma.jl
+++ src/core/sigma.jl
@@ -9,6 +9,10 @@
 
 """
     sigma_reset(lr::Logger)
+
+Create initial self-energy functions and write them to `sigma.bare`. 
+
+See also: [`sigma_dcount`](@ref).
 """
 function sigma_reset(lr::Logger)
     prompt(lr.log, "sigma::reset")
@@ -31,6 +35,14 @@ function sigma_reset(lr::Logger)
     # sigma = zeros(C64, nmesh, nsite)
 end
 
+"""
+    sigma_dcount(lr::Logger)
+
+Calculate double counting terms for self-energy functions and write
+them to `sigma.dc`.
+
+See also: [`sigma_reset`](@ref).
+"""
 function sigma_dcount(lr::Logger)
 end
 
