23655e2f6e2e49453a57e67f6da4744be432aa78
Li Huang <lihuang.dmft@gmail.com>
2021-03-28 23:19:17 +0800

    rename _run to _exec

--- src/core/dmft.jl
+++ src/core/dmft.jl
@@ -4,13 +4,13 @@
 # Author  : Li Huang (lihuang.dmft@gmail.com)
 # Status  : Unstable
 #
-# Last modified: 2021/02/09
+# Last modified: 2021/03/28
 #
 
 function dmft1_init() end
-function dmft1_run() end
+function dmft1_exec() end
 function dmft1_save() end
 
 function dmft2_init() end
-function dmft2_run() end
+function dmft2_exec() end
 function dmft2_save() end
