2787914719edbf71efa7f91c658d9edea462a9e2
Li Huang <lihuang.dmft@gmail.com>
2021-03-29 00:49:26 +0800

    fix solver_save()

--- src/core/base.jl
+++ src/core/base.jl
@@ -788,15 +788,19 @@ function solver_save(it::IterInfo)
         engine = get_s("engine")
         @cswitch engine begin
             @case "ct_hyb1"
+                sorry()
                 break
 
             @case "ct_hyb2"
+                sorry()
                 break
 
             @case "hub1"
+                sorry()
                 break
 
             @case "norg"
+                sorry()
                 break
 
             @default
