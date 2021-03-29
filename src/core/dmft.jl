ef440c36c8941fc50d4364030ee795a19a168f3d
Li Huang <lihuang.dmft@gmail.com>
2021-03-29 20:03:12 +0800

    add dmft_init(), dmft_exec(), and dmft_save()

--- src/core/dmft.jl
+++ src/core/dmft.jl
@@ -7,6 +7,26 @@
 # Last modified: 2021/03/29
 #
 
-function dmft_init() end
-function dmft_exec() end
-function dmft_save() end
+"""
+    dmft_init(it::IterInfo)
+
+See also: [`dmft_exec`](@ref), [`dmft_save`](@ref).
+"""
+function dmft_init(it::IterInfo)
+end
+
+"""
+    dmft_exec(it::IterInfo)
+
+See also: [`dmft_init`](@ref), [`dmft_save`](@ref).
+"""
+function dmft_exec(it::IterInfo)
+end
+
+"""
+    dmft_save(it::IterInfo)
+
+See also: [`dmft_init`](@ref), [`dmft_exec`](@ref).
+"""
+function dmft_save(it::IterInfo)
+end
