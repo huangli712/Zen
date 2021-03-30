87fb028928756c672a44c28a1ecedfddb53cf2ab
Li Huang <lihuang.dmft@gmail.com>
2021-03-30 10:40:30 +0800

    add s_hub1_init(), s_hub1_exec(), and s_hub1_save()

--- src/core/solver.jl
+++ src/core/solver.jl
@@ -7,11 +7,26 @@
 # Last modified: 2021/03/30
 #
 
+#
 # CT-HYB1
+#
 
+#
 # CT-HYB2
+#
 
+#
 # HUB-I
+#
+
+function s_hub1_init()
+end
+
+function s_hub1_exec()
+end
+
+function s_hub1_save()
+end
 
 #
 # NORG Quantum Impurity Solver
