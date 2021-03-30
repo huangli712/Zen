c526c0d73e5fb1b7b6737b4851b4610326a213da
Li Huang <lihuang.dmft@gmail.com>
2021-03-30 20:45:42 +0800

    add comments

--- src/core/solver.jl
+++ src/core/solver.jl
@@ -11,13 +11,31 @@
 # CT-HYB1 Quantum Impurity Solver
 #
 
+"""
+    s_qmc1_init()
+
+See also: [`s_qmc1_exec`](@ref), [`s_qmc1_save`](@ref).
+"""
 function s_qmc1_init()
+    sorry()
 end
 
+"""
+    s_qmc1_exec()
+
+See also: [`s_qmc1_init`](@ref), [`s_qmc1_save`](@ref).
+"""
 function s_qmc1_exec()
+    sorry()
 end
 
+"""
+    s_qmc1_save()
+
+See also: [`s_qmc1_init`](@ref), [`s_qmc1_exec`](@ref).
+"""
 function s_qmc1_save()
+    sorry()
 end
 
 #
@@ -25,12 +43,15 @@ end
 #
 
 function s_qmc2_init()
+    sorry()
 end
 
 function s_qmc2_exec()
+    sorry()
 end
 
 function s_qmc2_save()
+    sorry()
 end
 
 #
@@ -38,12 +59,15 @@ end
 #
 
 function s_hub1_init()
+    sorry()
 end
 
 function s_hub1_exec()
+    sorry()
 end
 
 function s_hub1_save()
+    sorry()
 end
 
 #
@@ -51,10 +75,13 @@ end
 #
 
 function s_norg_init()
+    sorry()
 end
 
 function s_norg_exec()
+    sorry()
 end
 
 function s_norg_save()
+    sorry()
 end
