e2a649f0a552f800b061302a95ba2c69f45aaa78
Li Huang <lihuang.dmft@gmail.com>
2021-04-04 23:29:35 +0800

    add dmft_setup_system()

--- src/dmft/dmft_stream.f90
+++ src/dmft/dmft_stream.f90
@@ -5,7 +5,7 @@
 !!! type    : subroutines
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
 !!! history : 02/23/2021 by li huang (created)
-!!!           03/17/2021 by li huang (last modified)
+!!!           04/04/2021 by li huang (last modified)
 !!! purpose :
 !!! status  : unstable
 !!! comment :
@@ -106,6 +106,12 @@
 
      return
   end subroutine dmft_setup_param
+
+  subroutine dmft_setup_system()
+     implicit none
+
+     return
+  end subroutine dmft_setup_system
 
   subroutine dmft_alloc_array()
      implicit none
