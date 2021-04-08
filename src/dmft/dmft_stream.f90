a197fdb028f1b37a7b53f7521abb46d2020f32cf
Li Huang <lihuang.dmft@gmail.com>
2021-04-08 16:24:41 +0800

    fix comments

--- src/dmft/dmft_stream.f90
+++ src/dmft/dmft_stream.f90
@@ -9,7 +9,7 @@
 !!! type    : subroutines
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
 !!! history : 02/23/2021 by li huang (created)
-!!!           04/07/2021 by li huang (last modified)
+!!!           04/08/2021 by li huang (last modified)
 !!! purpose :
 !!! status  : unstable
 !!! comment :
@@ -294,8 +294,8 @@
 !!
 !! @sub dmft_input_group
 !!
-!! read in groups of projectors. the data are used to embed or downfold
-!! the self-energy functions
+!! read in groups of projectors (see module dmft_group). the data are
+!! used to embed or downfold the self-energy functions
 !!
   subroutine dmft_input_group()
      use constants, only : mytmp
