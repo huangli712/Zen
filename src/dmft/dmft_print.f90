ed4f36aff067c91de4a59d85be747476476fc1cd
Li Huang <lihuang.dmft@gmail.com>
2021-04-12 20:39:43 +0800

    improve comments

--- src/dmft/dmft_print.f90
+++ src/dmft/dmft_print.f90
@@ -1,11 +1,13 @@
 !!!-----------------------------------------------------------------------
 !!! project : jacaranda
-!!! program :
+!!! program : dmft_print_header
+!!!           dmft_print_footer
+!!!           dmft_print_summary
 !!! source  : dmft_print.f90
 !!! type    : subroutines
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
 !!! history : 02/23/2021 by li huang (created)
-!!!           03/16/2021 by li huang (last modified)
+!!!           04/12/2021 by li huang (last modified)
 !!! purpose :
 !!! status  : unstable
 !!! comment :
