febe867362dc542b4c9fcc93c569931c045b2699
Li Huang <lihuang.dmft@gmail.com>
2021-04-13 20:08:17 +0800

    add more subroutines

--- src/dmft/dmft_core.f90
+++ src/dmft/dmft_core.f90
@@ -1,11 +1,13 @@
 !!!-----------------------------------------------------------------------
 !!! project : jacaranda
-!!! program :
+!!! program : dmft_driver
+!!!           map_chi_psi
+!!!           map_psi_chi
 !!! source  : dmft_core.f90
 !!! type    : subroutines
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
 !!! history : 02/23/2021 by li huang (created)
-!!!           04/12/2021 by li huang (last modified)
+!!!           04/13/2021 by li huang (last modified)
 !!! purpose :
 !!! status  : unstable
 !!! comment :
@@ -18,3 +20,15 @@
 
      return
   end subroutine dmft_driver
+
+  subroutine map_chi_psi()
+     implicit none
+
+     return
+  end subroutine map_chi_psi
+
+  subroutine map_psi_chi()
+     implicit none
+
+     return
+  end subroutine map_psi_chi
