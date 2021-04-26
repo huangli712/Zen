bad9779a5612add761e3ed8deeb163b2fe8adcdf
Li Huang <lihuang.dmft@gmail.com>
2021-04-27 00:01:54 +0800

    add functions

--- src/dmft/dmft_dump.f90
+++ src/dmft/dmft_dump.f90
@@ -5,7 +5,7 @@
 !!! type    : subroutines
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
 !!! history : 02/23/2021 by li huang (created)
-!!!           04/26/2021 by li huang (last modified)
+!!!           04/27/2021 by li huang (last modified)
 !!! purpose :
 !!! status  : unstable
 !!! comment :
@@ -13,3 +13,12 @@
 
   subroutine dmft_dump_sig_k()
   end subroutine dmft_dump_sig_k
+
+  subroutine dmft_dump_grn_k()
+  end subroutine dmft_dump_grn_k
+
+  subroutine dmft_dump_wss_k()
+  end subroutine dmft_dump_wss_k
+
+  subroutine dmft_dump_hyb_k()
+  end subroutine dmft_dump_hyb_k
