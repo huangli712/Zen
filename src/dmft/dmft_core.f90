327e24d37119c29f2259e386f7513f4f13d123a6
Li Huang <lihuang.dmft@gmail.com>
2021-04-29 10:38:20 +0800

    implement cat_grn_l()

--- src/dmft/dmft_core.f90
+++ src/dmft/dmft_core.f90
@@ -49,17 +49,22 @@
      integer :: cbnd, cdim
      integer :: bs, be
 
-     complex(dp) :: Gm(qdim,qdim)
-     complex(dp) :: Tm(qbnd,qbnd)
-     complex(dp) :: Hm(qbnd)
+     !complex(dp) :: Gm(qdim,qdim)
+     !complex(dp) :: Tm(qbnd,qbnd)
+     !complex(dp) :: Hm(qbnd)
 
-     Gm = czero
-     Tm = czero
-     Hm = czero
+     !Gm = czero
+     !Tm = czero
+     !Hm = czero
+
+     complex(dp), allocatable :: Gm(:,:)
+     complex(dp), allocatable :: Tm(:,:)
+     complex(dp), allocatable :: Hm(:)
+
      grn_l(:,:,:,:,t) = czero
-
      cbnd = 0
      cdim = ndim(t)
+     allocate(Gm(cdim,cdim))
 
      do s=1,nspin
          do k=1,nkpt
@@ -67,24 +72,30 @@
              be = kwin(k,s,2,t)
              cbnd = be - bs + 1
 
+             allocate(Hm(cbnd))
+             allocate(Tm(cbnd,cbnd))
              do m=1,nmesh
 
-                 Hm(1:cbnd) = czi * fmesh(m) + fermi - enk(bs:be,k,s)
-                 call s_diag_z(cbnd, Hm(1:cbnd), Tm(1:cbnd,1:cbnd))
+                 Hm = czi * fmesh(m) + fermi - enk(bs:be,k,s)
+                 call s_diag_z(cbnd, Hm, Tm)
+                 !Hm(1:cbnd) = czi * fmesh(m) + fermi - enk(bs:be,k,s)
+                 !call s_diag_z(cbnd, Hm(1:cbnd), Tm(1:cbnd,1:cbnd))
 
 ! add self-energy function here
 
-                 call s_inv_z(cbnd, Tm(1:cbnd,1:cbnd))
+                 call s_inv_z(cbnd, Tm)
 
-                 call map_psi_chi(cbnd, cdim, k, s, t, Tm(1:cbnd,1:cbnd), Gm(1:cdim,1:cdim))
-                 grn_l(1:cdim,1:cdim,m,s,t) = grn_l(1:cdim,1:cdim,m,s,t) + Gm(1:cdim,1:cdim)
+                 call map_psi_chi(cbnd, cdim, k, s, t, Tm, Gm)
+                 grn_l(1:cdim,1:cdim,m,s,t) = grn_l(1:cdim,1:cdim,m,s,t) + Gm
              enddo ! over m={1,nmesh} loop
-
+             deallocate(Hm)
+             deallocate(Tm)
          enddo ! over k={1,nkpt} loop
      enddo ! over s={1,nspin} loop
 
      grn_l = grn_l / float(nkpt)
      print *, grn_l(1:ndim(t),1:ndim(t), 1, 1, t) 
+     deallocate(Gm)
 
      return
   end subroutine cal_grn_l
