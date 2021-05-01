f0690f7eb11597faa67a39f19d7807c04c1adbe5
Li Huang <lihuang.dmft@gmail.com>
2021-05-01 10:40:46 +0800

    try to implement dmft_dump_grn_l()

--- src/dmft/dmft_dump.f90
+++ src/dmft/dmft_dump.f90
@@ -16,6 +16,8 @@
 !!
 !! @sub dmft_dump_grn_l
 !!
+!! write out local green's function in matsubara frequency space
+!!
   subroutine dmft_dump_grn_l(grn_l)
      use constants, only : dp
      use constants, only : mytmp
@@ -25,6 +27,7 @@
 
      use context, only : qdim
      use context, only : ndim
+     use context, only : fmesh
 
      implicit none
 
@@ -48,13 +51,29 @@
      write(mytmp,'(a9,i4)') '# nmesh: ', nmesh
      write(mytmp,'(a9,i4)') '# qdim : ', qdim
 
+! write separators
      write(mytmp,*)
      write(mytmp,*)
 
 ! write body
      do t=1,nsite
          do s=1,nspin
+
+! write data for given spin and site
              write(mytmp,'(a7,i4,2X,a5,i4)') "# site:", t, "spin:", s
+             do m=1,nmesh
+                 write(mytmp,'(a2,f16.8)') 'w:', fmesh(m)
+                 do q=1,ndim(t)
+                     do p=1,ndim(t)
+                         write(mytmp,'(2i4,2f16.8)') p, q, grn_l(p,q,m,s,t)
+                     enddo ! over p={1,ndim(t)} loop
+                 enddo ! over q={1,ndim(t)} loop
+             enddo ! over m={1,nmesh} loop
+
+! write separators
+             write(mytmp,*)
+             write(mytmp,*)
+
          enddo ! over s={1,nspin} loop
      enddo ! over t={1,nsite} loop
 
