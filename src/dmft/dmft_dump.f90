9cc578eeb3d035d521327ca73abbf92fe1a94cb6
Li Huang <lihuang.dmft@gmail.com>
2021-05-01 10:55:22 +0800

    implement dmft_dump_wss_l()

--- src/dmft/dmft_dump.f90
+++ src/dmft/dmft_dump.f90
@@ -86,8 +86,69 @@
 !!
 !! @sub dmft_dump_wss_l
 !!
-  subroutine dmft_dump_wss_l()
+!! write out local weiss's function in matsubara frequency space
+!!
+  subroutine dmft_dump_wss_l(wss_l)
+     use constants, only : dp
+     use constants, only : mytmp
+
+     use control, only : nspin
+     use control, only : nsite, nmesh
+
+     use context, only : qdim
+     use context, only : ndim
+     use context, only : fmesh
+
      implicit none
+
+! external arguments
+! local weiss's function
+     complex(dp), intent(in) :: wss_l(qdim,qdim,nmesh,nspin,nsite)
+
+! local variables
+! loop index
+     integer :: t
+     integer :: s
+     integer :: m
+     integer :: p, q
+
+! open data file: dmft_wss_l.dat
+     open(mytmp, file='dmft_wss_l.dat', form='formatted', status='unknown')
+
+! write parameters
+     write(mytmp,'(a9,i4)') '# nsite: ', nsite
+     write(mytmp,'(a9,i4)') '# nspin: ', nspin
+     write(mytmp,'(a9,i4)') '# nmesh: ', nmesh
+     write(mytmp,'(a9,i4)') '# qdim : ', qdim
+
+! write separators
+     write(mytmp,*)
+     write(mytmp,*)
+
+! write body
+     do t=1,nsite
+         do s=1,nspin
+
+! write data for given spin and site
+             write(mytmp,'(a7,i4,2X,a5,i4)') "# site:", t, "spin:", s
+             do m=1,nmesh
+                 write(mytmp,'(a2,i6,f16.8)') 'w:', m, fmesh(m)
+                 do q=1,ndim(t)
+                     do p=1,ndim(t)
+                         write(mytmp,'(2i4,2f16.8)') p, q, wss_l(p,q,m,s,t)
+                     enddo ! over p={1,ndim(t)} loop
+                 enddo ! over q={1,ndim(t)} loop
+             enddo ! over m={1,nmesh} loop
+
+! write separators
+             write(mytmp,*)
+             write(mytmp,*)
+
+         enddo ! over s={1,nspin} loop
+     enddo ! over t={1,nsite} loop
+
+! close data file
+     close(mytmp)
 
      return
   end subroutine dmft_dump_wss_l
