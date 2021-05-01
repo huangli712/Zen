a2a81ccbcfc35d13d48c03d0d4e804ce04f87df3
Li Huang <lihuang.dmft@gmail.com>
2021-05-01 10:25:01 +0800

    try to implement dmft_dump_grn_l()

--- src/dmft/dmft_dump.f90
+++ src/dmft/dmft_dump.f90
@@ -18,8 +18,32 @@
 !!
   subroutine dmft_dump_grn_l(grn_l)
      use constants, only : dp
+     use constants, only : mytmp
+
+     use control, only : nspin
+     use control, only : nsite, nmesh
+
+     use context, only : qdim
+     use context, only : ndim
 
      implicit none
+
+! external arguments
+! local green's function
+     complex(dp), intent(in) :: grn_l(qdim,qdim,nmesh,nspin,nsite)
+
+! local variables
+! loop index
+     integer :: t
+     integer :: s
+     integer :: m
+     integer :: p, q
+
+! open data file: solver.sgm.dat
+     open(mytmp, file='solver.sgm.dat', form='formatted', status='unknown')
+
+! close data file
+     close(mytmp)
 
      return
   end subroutine dmft_dump_grn_l
