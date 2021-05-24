b9aef81cf3873fa8708e2be141625c90997340c3
Li Huang <lihuang.dmft@gmail.com>
2021-05-24 10:32:36 +0800

    fix comments

--- src/core/ZenCore.jl
+++ src/core/ZenCore.jl
@@ -27,13 +27,13 @@ Zen supports the following quantum impurity solvers:
 * `HIA`
 * `NORG`
 
-For more details about how to install and use the Zen framework, please
-visit the following website:
-* `http://doc.zen`
-
 ZenCore implements the core library of the Zen computation framework. It
 connects various components of Zen, and drive them to work together. It
-provides an easy-to-use user and flexible user interface. 
+provides an easy-to-use user and flexible user interface.
+
+For more details about how to install and use the Zen framework and the
+ZenCore library, please visit the following website:
+* `http://doc.zen`
 """
 module Zen
 
