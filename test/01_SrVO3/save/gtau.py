ec87ce9b3db2a5069ae1b99c7cda03783eefbc09
Li Huang <lihuang.dmft@gmail.com>
2021-06-25 16:41:10 +0800

    remove save dir

--- test/01_SrVO3/save/gtau.py
+++
@@ -1,9 +0,0 @@
-from h5 import *
-from triqs.gf import *
-
-
-h = HDFArchive("vasp.h5", "r")
-gtau = h['dmft_output']['G_tau']['up_0']
-
-for i in range(10001):
-    print(i, " ", gtau.data[i,0,0].real, gtau.data[i,1,1].real, gtau.data[i,2,2].real, gtau.data[i,3,3].real, gtau.data[i,4,4].real)
