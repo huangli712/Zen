452a5a79d2f926d768a3cc4299af77f498c85c7b
Li Huang <lihuang.dmft@gmail.com>
2021-08-03 17:59:59 +0800

    fix make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,7 @@
 using Documenter
 
 makedocs(
-    sitename="Flink",
+    sitename="Zen",
     clean = false,
     authors = "Li Huang",
     format = Documenter.HTML(
@@ -10,28 +10,11 @@ makedocs(
     ),
     pages = [
         "Home" => "index.md",
-        "Introduction" => "intro.md",
-        "Installation" => "install.md",
-        "Basic usage" => "usage.md",
-        "User's guide" => Any[
-            "Constants" => "guide/m_constants.md",
-            "Linked list" => "guide/m_linkedlist.md",
-            "Message passaging interface" => "guide/m_mpi.md",
-            "Configuration parser" => "guide/m_parser.md",
-            "Sparse matrix" => "guide/m_sparse.md",
-            "Pseudorandom number generator" => "guide/m_spring.md",
-            "Stack" => "guide/m_stack.md",
-            "Analytical tetrahedron algorithm" => "guide/m_tetra.md",
-            "Error and exception" => "guide/s_error.md",
-            "Fourier transformation" => "guide/s_fourier.md",
-            "Special functions" => "guide/s_function.md",
-            "Integration" => "guide/s_integrator.md",
-            "Spline interpolation" => "guide/s_spline.md",
-            "Utility" => "guide/s_util.md",
-            "Vector" => "guide/s_vector.md",
-        ],
-        "Appendix" => Any[
-            "make.sys" => "appendix/make.md",
-        ],
+        "Introduction" => "intro/readme.md",
+        "Getting started" => "start/readme.md",
+        "Tutorials" => "tutor/readme.md",
+        "Guide" => "guide/readme.md",
+        "Internals" => "internals/readme.md",
+        "Theory" => "theory/readme.md",
     ],
 )
