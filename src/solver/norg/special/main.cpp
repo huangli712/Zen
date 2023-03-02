b4779dd376347ec9728a8fa35332f04427987c08
jm_office <jmw@ruc.edu.cn>
2023-03-02 16:15:40 +0800

    Version: v0.2.24 @ 2023.03.03

--- src/solver/norg/special/main.cpp
+++ src/solver/norg/special/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char* argv[])
 	using namespace std;
 	MPI_Init(&argc, &argv);
 	MyMpi mm;
-	if (mm) std::cout << "\n\n Version: v0.2.23 @ 2023.03.01 (running "<< present() <<")\n\n" << std::endl;
+	if (mm) std::cout << "\n\n Version: v0.2.24 @ 2023.03.03 (running "<< present() <<")\n\n" << std::endl;
 	if (mm) cout << NAV(pwd()) << endl; 
 	use_mkl(mm);
 	// if (mm) io_init();
